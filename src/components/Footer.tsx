572c3f17ed4f2241a268c7591460dacc7d2f88d4
kartikay <kartikaygautam24617@gmail.com>
2024-07-01 16:57:24 +0530

    Initialised

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <>
+      <div>Footer</div>
+      <Link to="/">Return Back</Link>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+function Navbar() {
+  return (
+    <div
+      id="navbar-main"
+      className="bg-green-500 w-full h-[120px] flex items-center justify-between px-[100px]"
+    >
+      <div id="playwrite-it-moderna-uniquifier">GoFood</div>
+    </div>
+  );
+}
+
+export default Navbar;
